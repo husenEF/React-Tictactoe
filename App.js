31a17fc20fe35e60473306514bb32a16ec147885
Husen Efendi <husenef@gmail.com>
2021-03-30 21:06:22 +0700

    update block

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 
 import {Block} from '@component';
 
 const Home = () => {
+  const [isCross, setCros] = useState(true);
+  const [dataCross, setDataCross] = useState([]);
+  const [dataCircle, setDataCircle] = useState([]);
+
+  const awnser = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 4, 9],
+    [3, 5, 7],
+  ];
+
+  const click = id => {
+    console.log({id});
+    if (isCross) {
+      const x = dataCross;
+      x.push(id);
+      setDataCross(x);
+    } else {
+      const c = dataCircle;
+      c.push(id);
+      setDataCircle(c);
+    }
+    togglePlayer();
+  };
+
+  const togglePlayer = () => setCros(!isCross);
+  const check = () => {};
+
+  useEffect(() => {
+    console.log({dataCircle, dataCross});
+  }, [dataCircle, dataCross]);
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Text>Home</Text>
-      <Block />
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <Block id={1} click={click} dataX={dataCross} dataC={dataCircle} />
+        <Block id={2} click={click} dataX={dataCross} dataC={dataCircle} />
+        <Block id={3} click={click} dataX={dataCross} dataC={dataCircle} />
+      </View>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <Block id={4} click={click} dataX={dataCross} dataC={dataCircle} />
+        <Block id={5} click={click} dataX={dataCross} dataC={dataCircle} />
+        <Block id={6} click={click} dataX={dataCross} dataC={dataCircle} />
+      </View>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <Block id={7} click={click} dataX={dataCross} dataC={dataCircle} />
+        <Block id={8} click={click} dataX={dataCross} dataC={dataCircle} />
+        <Block id={9} click={click} dataX={dataCross} dataC={dataCircle} />
+      </View>
     </View>
   );
 };
 
 const App = () => {
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Text>Main App</Text>
       <Home />
     </View>
--- src/component/block/index.js
+++ src/component/block/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
 
-const Block = () => {
+const Block = ({id, click, dataX, dataC}) => {
+  // const isDisable =
+  const isX = dataX.includes(id);
+  const isC = dataC.includes(id);
+  const isDisable = isX || isC ? true : false;
+  console.log({dataX, dataC});
   return (
-    <View>
-      <Text>Block</Text>
-    </View>
+    <TouchableOpacity
+      disabled={isDisable}
+      style={[Style.block, isX ? Style.bgX : null, isC ? Style.bgC : null]}
+      onPress={() => click(id)}>
+      <Text>Block - {id}</Text>
+    </TouchableOpacity>
   );
 };
 
 export default Block;
+
+const Style = StyleSheet.create({
+  block: {
+    borderWidth: 1,
+    borderColor: 'black',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bgX: {
+    backgroundColor: 'blue',
+  },
+  bgC: {
+    backgroundColor: 'pink',
+  },
+});
