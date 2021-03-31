c40df78a0d36abb26e4370daa27b08f0485f32ed
Husen Efendi <husenef@gmail.com>
2021-03-31 21:25:58 +0700

    baru design

--- src/screens/App2/index.js
+++ src/screens/App2/index.js
@@ -2,24 +2,38 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 class App2 extends Component {
+  state = {};
+  Icon = (s = 'X') => <Text style={{fontSize: 30}}>{s}</Text>;
   render() {
     return (
       <View style={Styles.container}>
         <Text>App 2</Text>
         <View style={Styles.boxContainer}>
-          <View style={Styles.box} />
-          <View style={Styles.box} />
-          <View style={Styles.box} />
+          <View style={[Styles.box, {borderLeftWidth: 0, borderTopWidth: 0}]}>
+            {this.Icon()}
+          </View>
+          <View style={[Styles.box, {borderTopWidth: 0}]}>{this.Icon()}</View>
+          <View style={[Styles.box, {borderRightWidth: 0, borderTopWidth: 0}]}>
+            {this.Icon()}
+          </View>
         </View>
         <View style={Styles.boxContainer}>
-          <View style={Styles.box} />
-          <View style={Styles.box} />
-          <View style={Styles.box} />
+          <View style={[Styles.box, {borderLeftWidth: 0}]}>{this.Icon()}</View>
+          <View style={[Styles.box]}>{this.Icon()}</View>
+          <View style={[Styles.box, {borderRightWidth: 0}]}>{this.Icon()}</View>
         </View>
         <View style={Styles.boxContainer}>
-          <View style={Styles.box} />
-          <View style={Styles.box} />
-          <View style={Styles.box} />
+          <View
+            style={[Styles.box, {borderLeftWidth: 0, borderBottomWidth: 0}]}>
+            {this.Icon()}
+          </View>
+          <View style={[Styles.box, {borderBottomWidth: 0}]}>
+            {this.Icon()}
+          </View>
+          <View
+            style={[Styles.box, {borderBottomWidth: 0, borderRightWidth: 0}]}>
+            {this.Icon()}
+          </View>
         </View>
       </View>
     );
@@ -39,9 +53,11 @@ const Styles = StyleSheet.create({
     flexDirection: 'row',
   },
   box: {
-    borderWidth: 1,
+    borderWidth: 3,
     borderColor: 'black',
     height: 100,
     width: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
